0c92031d8bc5d9a47f55fc116cff28c3c3cf1a67
DESKTOP-8PSQBN9\PC <dinhphuongnam987@gmail.com>
2022-11-15 00:01:07 +0700

    b04-c07 Contact Email

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\SettingModel;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +26,25 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if(Schema::hasTable('setting')) {
+            $model = new SettingModel();
+            $mail = $model->getItem(['field' => 'key_value', 'field_value' => 'email-setting'], ['task' => 'get-item']);
+            
+            if($mail) {
+                $data = [
+                    'driver' => 'smtp',
+                    'host' => 'smtp.gmail.com',
+                    'port' => 587,
+                    'encryption' => 'tls',
+                    'username' => $mail['email'],
+                    'password' => $mail['password'],
+                    'from' => [
+                        'address' => $mail['email'],
+                        'name'  => 'news.vn'
+                    ]
+                ];
+                Config::set('mail', $data);
+            }
+        }
     }
 }
---
+++ resources/views/mail.blade.php
@@ -0,0 +1,2 @@
+<h1>Cảm ơn bạn đã gửi thông tin liên hệ!</h1>
+<h3>Chúng tôi sẽ liên lạc đến bạn trong thời gian sớm nhất</h3>
