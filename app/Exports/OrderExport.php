dba8c088bd59dbb843ce649beb34b35d9f2bda78
DESKTOP-8PSQBN9\PC <dinhphuongnam987@gmail.com>
2022-12-23 08:53:55 +0700

    Export Order: Fix the size excel column when export

--- app/Exports/OrderExport.php
+++ app/Exports/OrderExport.php
@@ -5,8 +5,9 @@ namespace App\Exports;
 use App\Models\OrderModel;
 use Illuminate\Contracts\View\View;
 use Maatwebsite\Excel\Concerns\FromView;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 
-class OrderExport implements FromView
+class OrderExport implements FromView, ShouldAutoSize
 {
     public function view(): View
     {
--- resources/views/admin/pages/order/list.blade.php
+++ resources/views/admin/pages/order/list.blade.php
@@ -63,11 +63,11 @@
                 @endif
             </tbody>
         </table>
-        <button class="btn btn-warning" style="float: right; margin-right: 30px">
-            <a href="{{ route($controllerName . '/export') }}" target="_blank" style="color: white">
+        <a href="{{ route($controllerName . '/export') }}" target="_blank" style="color: white">
+            <button class="btn btn-warning" style="float: right; margin-right: 30px">
                 Xuất danh sách đơn hàng(Excel)
-            </a>
-        </button>
+            </button>
+        </a>
     </div>
 </div>
            
