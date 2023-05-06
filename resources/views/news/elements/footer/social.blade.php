9d7aaf119b25b5855193a5b21e0ad75ffbcf5feb
dinhphuongnam987 <dinhphuongnam987@gmail.com>
2023-05-06 20:33:17 +0700

    Edit social footer

--- resources/views/news/elements/footer/social.blade.php
+++ resources/views/news/elements/footer/social.blade.php
@@ -6,11 +6,11 @@
 <div class="col-md-3 social">
     <span class="title">Social</span>
     <div class="item">
-        <i class="fa fa-map-marker icon" aria-hidden="true"></i>
+        <i class="fa fa-facebook icon" aria-hidden="true"></i>
         <span>{{ $facebook }}</span>
     </div>
     <div class="item">
-        <i class="fa fa-clock-o icon" aria-hidden="true"></i>
-        <span>{{ $facebook }}</span>
+        <i class="fa fa-youtube icon" aria-hidden="true"></i>
+        <span>{{ $youtube }}</span>
     </div>
 </div>
